1f19cb7b125d31651a6ef700627793332d3ff456
Md Repon <repon7253@gmail.com>
2025-07-03 12:58:15 +0530

    refactor: simplify AddBookModal by removing unused props and edit functionality

--- src/components/module/addBook/addBookModal.tsx
+++ src/components/module/addBook/addBookModal.tsx
@@ -1,13 +1,5 @@
 import { Button } from '@/components/ui/button';
-import {
-  Dialog,
-  DialogClose,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-} from '@/components/ui/dialog';
+
 import {
   Form,
   FormControl,
@@ -17,19 +9,17 @@ import {
 } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
 import { Textarea } from '@/components/ui/textarea';
-import { useAddBookMutation, useEditBookMutation } from '@/redux/api/baseApi';
+import { useAddBookMutation } from '@/redux/api/baseApi';
 import type { IBook, IBookFormData } from '@/types';
-import { useEffect } from 'react';
 import { useForm } from 'react-hook-form';
+import { useNavigate } from 'react-router-dom';
 import { toast } from 'sonner';
 
 type Props = {
-  open: boolean;
-  setOpen: (value: boolean) => void;
   defaultValues?: IBook | null; // null means add, otherwise edit
 };
 
-const AddBookModal = ({ open, setOpen, defaultValues }: Props) => {
+const AddBookModal = ({ defaultValues }: Props) => {
   const form = useForm<IBookFormData>({
     defaultValues: {
       title: '',
@@ -40,31 +30,19 @@ const AddBookModal = ({ open, setOpen, defaultValues }: Props) => {
       copies: 0,
     },
   });
+  const navigate = useNavigate();
   const [addBook] = useAddBookMutation();
-  const [editBook] = useEditBookMutation();
-
-  // Pre-fill when editing
-  useEffect(() => {
-    if (defaultValues) {
-      form.reset(defaultValues);
-    }
-  }, [defaultValues, form]);
 
   const onSubmit = async (data: IBookFormData) => {
     try {
       data.available = true;
       data.copies = Number(data.copies);
 
-      if (defaultValues?._id) {
-        await editBook({ id: defaultValues._id, data }).unwrap();
-        toast.success('Book updated successfully!');
-      } else {
-        await addBook(data).unwrap();
-        toast.success('Book added successfully!');
-      }
+      await addBook(data).unwrap();
+      toast.success('Book added successfully!');
+      navigate('/books');
 
       form.reset();
-      setOpen(false);
     } catch (error) {
       toast.error('Failed to add book.');
       console.log(error);
@@ -73,105 +51,91 @@ const AddBookModal = ({ open, setOpen, defaultValues }: Props) => {
 
   return (
     <div>
-      {' '}
-      <Dialog open={open} onOpenChange={setOpen}>
-        <DialogContent className='sm:max-w-[425px]'>
-          <DialogHeader>
-            <DialogTitle>Add Book</DialogTitle>
-            <DialogDescription className='sr-only'>add book</DialogDescription>
-          </DialogHeader>
-          <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-4'>
-              <FormField
-                control={form.control}
-                name='title'
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Title</FormLabel>
-                    <FormControl>
-                      <Input placeholder='Title' {...field} />
-                    </FormControl>
-                  </FormItem>
-                )}
-              />
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-4'>
+          <FormField
+            control={form.control}
+            name='title'
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Title</FormLabel>
+                <FormControl>
+                  <Input placeholder='Title' {...field} />
+                </FormControl>
+              </FormItem>
+            )}
+          />
 
-              <FormField
-                control={form.control}
-                name='author'
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Author</FormLabel>
-                    <FormControl>
-                      <Input placeholder='Author' {...field} />
-                    </FormControl>
-                  </FormItem>
-                )}
-              />
+          <FormField
+            control={form.control}
+            name='author'
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Author</FormLabel>
+                <FormControl>
+                  <Input placeholder='Author' {...field} />
+                </FormControl>
+              </FormItem>
+            )}
+          />
 
-              <FormField
-                control={form.control}
-                name='genre'
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Genre</FormLabel>
-                    <FormControl>
-                      <Input placeholder='Genre' {...field} />
-                    </FormControl>
-                  </FormItem>
-                )}
-              />
+          <FormField
+            control={form.control}
+            name='genre'
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Genre</FormLabel>
+                <FormControl>
+                  <Input placeholder='Genre' {...field} />
+                </FormControl>
+              </FormItem>
+            )}
+          />
 
-              <FormField
-                control={form.control}
-                name='isbn'
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>ISBN</FormLabel>
-                    <FormControl>
-                      <Input placeholder='ISBN' {...field} />
-                    </FormControl>
-                  </FormItem>
-                )}
-              />
+          <FormField
+            control={form.control}
+            name='isbn'
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>ISBN</FormLabel>
+                <FormControl>
+                  <Input placeholder='ISBN' {...field} />
+                </FormControl>
+              </FormItem>
+            )}
+          />
 
-              <FormField
-                control={form.control}
-                name='description'
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Description</FormLabel>
-                    <FormControl>
-                      <Textarea placeholder='Description' {...field} />
-                    </FormControl>
-                  </FormItem>
-                )}
-              />
+          <FormField
+            control={form.control}
+            name='description'
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Description</FormLabel>
+                <FormControl>
+                  <Textarea placeholder='Description' {...field} />
+                </FormControl>
+              </FormItem>
+            )}
+          />
 
-              <FormField
-                control={form.control}
-                name='copies'
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Copies</FormLabel>
-                    <FormControl>
-                      <Input type='number' placeholder='Copies' {...field} />
-                    </FormControl>
-                  </FormItem>
-                )}
-              />
+          <FormField
+            control={form.control}
+            name='copies'
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Copies</FormLabel>
+                <FormControl>
+                  <Input type='number' placeholder='Copies' {...field} />
+                </FormControl>
+              </FormItem>
+            )}
+          />
 
-              <DialogFooter>
-                <DialogClose asChild>
-                  <Button variant='outline'>Cancel</Button>
-                </DialogClose>
-                <Button type='submit'>
-                  {defaultValues?._id ? 'Update Book' : 'Add Book'}
-                </Button>
-              </DialogFooter>
-            </form>
-          </Form>
-        </DialogContent>
-      </Dialog>
+          <Button type='submit'>
+            {defaultValues?._id ? 'Update Book' : 'Add Book'}
+          </Button>
+        </form>
+      </Form>
     </div>
   );
 };
